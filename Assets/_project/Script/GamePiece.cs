2f07ed035f99907766d94265b25fd8faefc541de
Senjucasie <paulkpraveen@gmail.com>
2023-09-08 16:09:55 +0530

    CREATED LOGIC FOR SMMOTH STEP TO MOVE THE GAME PIECES

--- Assets/_project/Script/GamePiece.cs
+++ Assets/_project/Script/GamePiece.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
-    [field:SerializeField]public int XIndex { get; private set; }
-    [field:SerializeField]public int YIndex { get; private set; }
+    [field: SerializeField] public int XIndex { get; private set; }
+    [field: SerializeField] public int YIndex { get; private set; }
 
     private Coroutine _moveCoroutine;
+
+    [SerializeField] private InterpolationType _interpolationType;
+    public enum InterpolationType { Linear, EaseIn, EaseOut, SmoothStep,SmootherStep };
 
     public void SetIndex(int x, int y)
     {
@@ -39,11 +42,43 @@ public class GamePiece : MonoBehaviour
         bool reachdestination = false;
         Vector3 startpos = transform.position;
         float elapsedtime = 0;
-        while(!reachdestination)
-        {
-            elapsedtime += Time.deltaTime;
-            float t = elapsedtime / timetomove;
-            transform.position = Vector3.Lerp(startpos, destination, t);
+        //ease in squared exponantial formula
+        //t = t * t;
+
+        //ease out squared exponantial formula
+        //t = 1 - Mathf.Pow(1 - t, 2);
+
+        //ease in and out smooth step https://en.wikipedia.org/wiki/Smoothstep
+        //t = (3 * Mathf.Pow(t,2)) - (2 * Mathf.Pow(t,3));
+
+        //ease in and out smoother step variation https://en.wikipedia.org/wiki/Smoothstep
+        //t = (6 * Mathf.Pow(t, 5)) - (15 * Mathf.Pow(t, 4))+(10*Mathf.Pow(t,3));
+        while (!reachdestination)
+            {
+                elapsedtime += Time.deltaTime;
+                float t = elapsedtime / timetomove;
+                switch(_interpolationType)
+                {
+                    case InterpolationType.Linear:
+                        break;
+                    case InterpolationType.EaseIn:
+                        t = t * t;
+                        break;
+                    case InterpolationType.EaseOut:
+                        t = 1 - Mathf.Pow(1 - t, 2);
+                        break;
+                    case InterpolationType.SmoothStep:
+                        t = (3 * Mathf.Pow(t, 2)) - (2 * Mathf.Pow(t, 3));
+                        break;
+                    case InterpolationType.SmootherStep:
+                        t = (6 * Mathf.Pow(t, 5)) - (15 * Mathf.Pow(t, 4)) + (10 * Mathf.Pow(t, 3));
+                        break;
+                }
+           
+
+
+
+    transform.position = Vector3.Lerp(startpos, destination, t);
 
             if (Vector2.Distance(transform.position,destination)<0.01f)
             {
